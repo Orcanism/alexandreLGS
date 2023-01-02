fa0f78fde87f882fff90effe5bc936a37f81e644
Orcanism <louis.orca.brunner@gmail.com>
2023-01-03 00:16:10 +0100

    mise ajour du nombre de messages

--- index.js
+++ index.js
@@ -110,7 +110,7 @@ client.on('ready', () => {
 client.on('guildMemberAdd', member => {
 	// Envoi un message en mp au nouveau membre
 	if (!memberStats.hasOwnProperty(member.id)) {
-		memberStats[member.id] = {"username": member.displayName, "messageCount": 0, "firstJoinDate": formatDate(new Date())};
+		memberStats[member.id] = {"username": member.displayName, "messageCount": 1, "firstJoinDate": formatDate(new Date())};
 		let memberStatsPush = JSON.stringify(memberStats, null, 4);
 		fs.writeFile("./memberStats.json", memberStatsPush, () => console.error);
 	}
