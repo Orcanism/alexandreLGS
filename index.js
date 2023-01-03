19baf29c3a96c31ff4fbe9a297390ef685502cba
Orcanism <louis.orca.brunner@gmail.com>
2023-01-03 14:59:59 +0100

    commande leaderboard operationnel

--- index.js
+++ index.js
@@ -329,7 +329,21 @@ client.on('messageCreate', msg => {
 
 			// Commande leaderboard, envoie les 3 membres les plus actifs du serveur
 			else if (cmd === 'leaderboard') {
-
+				let leaderboardArray = [];
+				for (let i = 0; i < Object.keys(memberStats).length; i++) {
+					let arrayKey = Object.keys(memberStats)[i];
+					leaderboardArray.push(memberStats[arrayKey]);
+				}
+				leaderboardArray = leaderboardArray.sort((a, b) => {
+					if (b.messageCount < a.messageCount) {
+					  	return -1;
+					}
+				}).slice(0, 4);
+				let firstPlace = ':first_place: **- ' + leaderboardArray[0].username + '** avec **' + leaderboardArray[0].messageCount + '** messages !';
+				let secondPlace = ':second_place: **- ' + leaderboardArray[1].username + '** avec **' + leaderboardArray[1].messageCount + '** messages !';
+				let thirdPlace = ':third_place: **- ' + leaderboardArray[2].username + '** avec **' + leaderboardArray[2].messageCount + '** messages !';
+				let fourthPlace = ':chocolate_bar: **- ' + leaderboardArray[3].username + '** avec **' + leaderboardArray[3].messageCount + '** messages !';
+				msg.channel.send('Voici les knacks les plus actives du serveur !\n\n' + firstPlace + '\n' + secondPlace + '\n' + thirdPlace + '\n' + fourthPlace);
 			}
 
 			// Commande me, envoie les informations de l'utilisateur qui appelle la commande
